d32876eda748c4fb1502816b4b79d7db133e80b3
vento1982 <marcin.vento@gmail.com>
2017-05-21 19:46:21 +0200

    Add Interface and Filter for Products

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,13 +1,14 @@
-import { Component }  from '@angular/core';
+import { Component, OnInit }  from '@angular/core';
 
-import { IProduct } from './product'
+import { IProduct } from './product';
 
 @Component({
 	selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
-    
+	moduleId: module.id,
+    templateUrl: 'product-list.component.html',
+    styleUrls: ['product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
 	pageTitle: string = 'Product list';
 	imageWidth: number = 50;
 	imageMargin: number = 2;
@@ -38,5 +39,8 @@ export class ProductListComponent{
 
 	toggleImage(): void{
 		this.showImage = !this.showImage;
+	}
+	ngOnInit(){
+		console.log('Oninit');
 	}
 }
